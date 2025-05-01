4c483a584926cf867efe10270e006a2224dc450b
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2025-05-01 13:49:14 -0300

    fix: enables CORS for Vercel's domain

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,12 +13,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "https://furia-chatbot-eta.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 # Registra as rotas
